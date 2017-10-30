6aa3476835d9bb910cab2d8da5778a21f4ec4bad
Lauren OMeara <jantzl@gmail.com>
2017-10-30 15:27:51 -0500

    added already logged screen

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
+import TextButton from './TextButton'
+import { Ionicons } from '@expo/vector-icons'
 
 function SubmitBtn ({ onPress }) {
 	return (
@@ -72,8 +74,34 @@ export default class AddEntry extends Component {
 		// cear local notification
 	}
 
+	reset = () => {
+		const key = timeToString()
+
+		// update redux
+		
+		// route to home
+		
+		// Update db
+		
+	}
+
 	render () {
 		const metaInfo = getMetricMetaInfo()
+
+		if (this.props.alreadyLogged) {
+			return (
+				<View>
+					<Ionicons
+						name='ios-happy-outline'
+						size={100}
+					/>
+					<Text>You already logged your information for today</Text>
+					<TextButton onPress={this.reset}>
+						Reset
+					</TextButton>
+				</View>
+			)
+		}
 
 		return (
 			<View>
