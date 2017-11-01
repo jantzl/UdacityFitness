f8b80a3c945f0f5a8eef6b58d3c84e25e64380bc
Lauren OMeara <jantzl@gmail.com>
2017-10-31 21:36:49 -0500

    added stack nav for entry detail

--- components/History.js
+++ components/History.js
@@ -38,7 +38,10 @@ class History extends Component {
 						</Text>
 					</View>
 				: <TouchableOpacity 
-						onPress={() => console.log('Pressed!')}>
+						onPress={() => this.props.navigation.navigate(
+							'EntryDetail',
+							{ entryId: key }
+						)}>
 						<MetricCard metrics={metrics} date={formattedDate} />
 					</TouchableOpacity>}
 		</View>
