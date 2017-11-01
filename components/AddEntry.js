b071b429c7019d33ed4d36b156576cc3072ab4f6
Lauren OMeara <jantzl@gmail.com>
2017-11-01 13:38:52 -0500

    added notification support

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -9,7 +9,9 @@ import {
 import { 
 	getMetricMetaInfo, 
 	timeToString, 
-	getDailyReminderValue 
+	getDailyReminderValue,
+	clearLocalNotification,
+	setLocalNotification
 } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
@@ -91,7 +93,8 @@ class AddEntry extends Component {
 
 		submitEntry({key, entry})
 
-		// clear local notification
+		clearLocalNotification()
+		.then(setLocalNotification)
 	}
 
 	reset = () => {
